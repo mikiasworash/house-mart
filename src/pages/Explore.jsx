43b3e58333440330649a7a707f5f7778bacef3c7
Mikias <mikias.worash.a@gmail.com>
2022-07-19 10:43:16 +0300

    Explore Page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 function Explore() {
   return (
-    <>
-      <h1>Explore</h1>
-    </>
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+
+      <main>
+        {/* {slider} */}
+
+        <p className="exploreCategoryHeading">Catergories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+          <Link to="/category/sell">
+            <img
+              src={sellCategoryImage}
+              alt="sell"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for sell</p>
+          </Link>
+        </div>
+      </main>
+    </div>
   );
 }
 
